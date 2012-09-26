2ce86c0a9f1d9d71c3f4719b4be64ea76c240511
Pavel Strybuk <pstr@tut.by>
2012-09-26 23:15:48 +0300

    Made tiny refactoring. Small fixes.

--- LoremIpsum.m
+++ LoremIpsum.m
@@ -10,6 +10,7 @@
 
 @interface LoremIpsum ()
 - (NSString*) randomWord;
+- (NSString*) capitalizeFirstLetterIn:(NSString*)text;
 @end
 
 @implementation LoremIpsum
@@ -17,6 +18,7 @@
 - (id) init {
   if ((self = [super init]))  {
       NSString *wordsList = @"a ac accumsan ad adipiscing aenean aliquam aliquet amet ante aptent arcu at auctor augue bibendum blandit class commodo condimentum congue consectetuer consequat conubia convallis cras cubilia cum curabitur curae; cursus dapibus diam dictum dictumst dignissim dis dolor donec dui duis egestas eget eleifend elementum elit enim erat eros est et etiam eu facilisis fames faucibus felis fermentum feugiat fusce gravida habitant habitasse hac hendrerit hymenaeos iaculis id imperdiet in inceptos integer interdum ipsum justo lacinia lacus laoreet lectus leo libero ligula litora lobortis lorem luctus magna magnis malesuada massa mattis mauris metus mi molestie mollis montes morbi mus nam nascetur natoque nec neque netus nibh nisi nisl non nonummy nostra nulla nullam nunc odio orci ornare parturient pede pellentesque penatibus per pharetra phasellus placerat platea porta porttitor posuere potenti praesent pretium primis proin pulvinar purus quam quis quisque rhoncus ridiculus risus rutrum sagittis sapien scelerisque sed sem semper senectus sit sociis sociosqu sodales sollicitudin suscipit suspendisse taciti tellus tempor tempus tincidunt torquent tortor tristique turpis ullamcorper ultrices ultricies urna ut varius vehicula vel velit venenatis vestibulum vitae vivamus viverra volutpat vulputate";
+      
       _words = [wordsList componentsSeparatedByString:@" "];
   }
   return self;
@@ -32,33 +34,38 @@
   return [_words objectAtIndex:randomIndex];
 }
 
+- (NSString*) capitalizeFirstLetterIn:(NSString*)text {
+    NSMutableString *_text = [text mutableCopy];
+    NSString *firstChar = [_text substringWithRange:NSMakeRange(0, 1)];
+    firstChar = [firstChar uppercaseString];
+    [_text replaceCharactersInRange:NSMakeRange(0, 1) withString:firstChar];
+
+    return _text;
+}
+
 #pragma mark -
 
 - (NSString*) words:(NSUInteger)count {
-  if (count==0) return @"";
+  if (count == 0) return @"";
   
   NSString* collectedWords = [self randomWord];
-  for (NSUInteger i=0; i<count; i++) {
+  for (NSUInteger i = 0; i < count; i++) {
     collectedWords = [collectedWords stringByAppendingString:@" "];
     collectedWords = [collectedWords stringByAppendingString:[self randomWord]];
   }
   
-  NSString* words = [collectedWords copy];
-  
-  return words;
+  return collectedWords;
 }
 
 - (NSString*) sentences:(NSUInteger)count {
     NSMutableString *result = [NSMutableString string];
     for (NSUInteger i = 0; i < count; i++) {
         long numberOfWords = random() % 10 + 10; //range from 10-20 words
-        NSMutableString *sentence = [[self words:numberOfWords] mutableCopy];
-        NSString *firstChar = [sentence substringWithRange:NSMakeRange(0, 1)];
-        firstChar = [firstChar uppercaseString];
-        [sentence replaceCharactersInRange:NSMakeRange(0, 1) withString:firstChar];
+
+        NSString *sentence = [self capitalizeFirstLetterIn:[self words:numberOfWords]];
         [result appendString:sentence];
         
-        if (i+1 == count) { //last sentence
+        if (i + 1 == count) { //last sentence
             [result appendString:@"."];
         } else {            
             [result appendString:@". "];
